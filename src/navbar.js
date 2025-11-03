29ad54814e2827fd48b1b877dce0478d5e11b905
grcubes <gustavosamrod@gmail.om>
2025-11-02 16:14:44 -0800

    :fix: Add functionality back to navbar buttons

---
+++ src/navbar.js
@@ -0,0 +1,23 @@
+export function setupNavbar()
+{
+    console.log("navbar setup triggered");
+
+    const navLinks = {
+        "logs-button" : "foodlog.html",
+        "addfood-button": "addfood.html",
+        "dashboard-button": "main.html",
+        "reminders-button": "reminder.html",
+        "profile-button": "profile.html"
+    };
+
+    for (let [id, target] of Object.entries(navLinks))
+    {
+        const button = document.getElementById(id);
+        if (!button) continue;
+
+        button.addEventListener("click", () => 
+        {
+            location.href = target;
+        })
+    }
+}
